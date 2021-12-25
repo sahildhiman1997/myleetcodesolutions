6ffdfea72fa1736706981f43d78b94ff0e219e4a
Sahil Dhiman <sahildhiman1997@gmail.com>
2021-12-25 20:27:53 +0530

    Time: 487 ms (55.41%), Space: 0B (100.00%) - LeetHub

--- reformat-department-table/reformat-department-table.sql
+++ reformat-department-table/reformat-department-table.sql
@@ -1,20 +1,20 @@
 # Write your MySQL query statement below
 SELECT 
 id,
-sum(case when month='Jan' then revenue else null end) as 'Jan_Revenue',
-sum(case when month='Feb' then revenue else null end) as 'Feb_Revenue',
-sum(case when month='Mar' then revenue else null end) as 'Mar_Revenue',
-sum(case when month='Apr' then revenue else null end) as 'Apr_Revenue',
-sum(case when month='May' then revenue else null end) as 'May_Revenue',
-sum(case when month='Jun' then revenue else null end) as 'Jun_Revenue',
-sum(case when month='Jul' then revenue else null end) as 'Jul_Revenue',
-sum(case when month='Aug' then revenue else null end) as 'Aug_Revenue',
-sum(case when month='Sep' then revenue else null end) as 'Sep_Revenue',
-sum(case when month='Oct' then revenue else null end) as 'Oct_Revenue',
-sum(case when month='Nov' then revenue else null end) as 'Nov_Revenue',
-sum(case when month='Dec' then revenue else null end) as 'Dec_Revenue'
+sum(case when month='Jan' then revenue end) as 'Jan_Revenue',
+sum(case when month='Feb' then revenue  end) as 'Feb_Revenue',
+sum(case when month='Mar' then revenue end) as 'Mar_Revenue',
+sum(case when month='Apr' then revenue  end) as 'Apr_Revenue',
+sum(case when month='May' then revenue  end) as 'May_Revenue',
+sum(case when month='Jun' then revenue  end) as 'Jun_Revenue',
+sum(case when month='Jul' then revenue  end) as 'Jul_Revenue',
+sum(case when month='Aug' then revenue  end) as 'Aug_Revenue',
+sum(case when month='Sep' then revenue  end) as 'Sep_Revenue',
+sum(case when month='Oct' then revenue  end) as 'Oct_Revenue',
+sum(case when month='Nov' then revenue  end) as 'Nov_Revenue',
+sum(case when month='Dec' then revenue  end) as 'Dec_Revenue'
 
 
 FROM Department
 group by id
-order by id;
+# order by id;
