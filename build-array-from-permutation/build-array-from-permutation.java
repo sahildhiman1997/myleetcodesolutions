544a88c3609b5e57af2c5e93e60bd5b5a19a9cd7
Sahil Dhiman <sahildhiman1997@gmail.com>
2021-12-25 09:04:36 +0530

    Update build-array-from-permutation.java
    added solution using O(1) space.

--- build-array-from-permutation/build-array-from-permutation.java
+++ build-array-from-permutation/build-array-from-permutation.java
@@ -1,9 +1,26 @@
 class Solution {
-     static public int[] buildArray(int[] nums) {
+     static public int[] buildArrayWithExtraSpace(int[] nums) {
         int[] myarr = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             myarr[i] = nums[nums[i]];
         }
         return myarr;
     }
+     
+     
+//      solution without using extra space.
+      static public int[] buildArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+            nums[i] += (nums[nums[i]]%nums.length)*nums.length;
+        }
+        for (int i = 0; i < nums.length; i++) {
+
+            nums[i] /= nums.length;
+        }
+
+
+        return nums;
+    }
+     
 }
