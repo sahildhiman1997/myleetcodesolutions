21ad9b98696d9f63f6a2a15341968039a1317470
Sahil Dhiman <sahildhiman1997@gmail.com>
2021-12-31 10:29:45 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (56.04%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.java
+++ deepest-leaves-sum/deepest-leaves-sum.java
@@ -14,43 +14,28 @@
  * }
  */
 class Solution {
-         public int deepestLeavesSum(TreeNode root) {
-            List<TreeNode> list1 = new ArrayList<TreeNode>();
-            list1.add(root);
-            List<TreeNode> list2 = new ArrayList<TreeNode>();
+  int maxDepth = 0;
+    int currentDepth = 0;
+    int sum = 0;
 
-            while (true) {
-                boolean listIsLast = false;
-                if (list2.isEmpty()) {
-                    for (TreeNode node : list1) {
-                        if (node.left != null) list2.add(node.left);
-                        if (node.right != null) list2.add(node.right);
-                    }
-                    if (list2.isEmpty() == false) {
-                        list1 = new ArrayList<TreeNode>();
-                    } else {
-                        break;
-                    }
-                } else if (list1.isEmpty()) {
-                    for (TreeNode node : list2) {
-                        if (node.left != null) list1.add(node.left);
-                        if (node.right != null) list1.add(node.right);
-                    }
-                    if (list1.isEmpty() == false) {
-                        list2 = new ArrayList<TreeNode>();
-                    } else {
-                        break;
-                    }
-                }
+  public int deepestLeavesSum(TreeNode root) {
+        int depth = currentDepth;
+        currentDepth = depth + 1;
+        if (root.left == null && root.right == null) {
+            if (maxDepth == depth) {
+                sum += root.val;
+            } else if (depth > maxDepth) {
+                sum = root.val;
+                maxDepth = depth;
             }
-            int answer = 0;
-            for (TreeNode node : list1) {
-                answer += node.val;
-            }
-            for (TreeNode node : list2) {
-                answer += node.val;
-            }
-
-            return answer;
+        } 
+        if (root.right != null) {
+            deepestLeavesSum(root.right);
         }
+        if (root.left != null) {
+            deepestLeavesSum(root.left);
+        }
+        currentDepth -= 1;
+        return sum;
+    }
 }
