f03772fd386adee7b5c1013c7a3e4614205acf71
Sahil Dhiman <sahildhiman1997@gmail.com>
2021-12-31 10:30:55 +0530

    Time: 3 ms (61.41%), Space: 51.7 MB (16.15%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.java
+++ deepest-leaves-sum/deepest-leaves-sum.java
@@ -18,6 +18,11 @@ class Solution {
     int currentDepth = 0;
     int sum = 0;
 
+    
+    
+    
+    
+//     using dfs
   public int deepestLeavesSum(TreeNode root) {
         int depth = currentDepth;
         currentDepth = depth + 1;
@@ -38,4 +43,47 @@ class Solution {
         currentDepth -= 1;
         return sum;
     }
+    
+    
+    // using bfs
+      public int deepestLeavesSumBFS(TreeNode root) {
+            List<TreeNode> list1 = new ArrayList<TreeNode>();
+            list1.add(root);
+            List<TreeNode> list2 = new ArrayList<TreeNode>();
+
+            while (true) {
+                boolean listIsLast = false;
+                if (list2.isEmpty()) {
+                    for (TreeNode node : list1) {
+                        if (node.left != null) list2.add(node.left);
+                        if (node.right != null) list2.add(node.right);
+                    }
+                    if (list2.isEmpty() == false) {
+                        list1 = new ArrayList<TreeNode>();
+                    } else {
+                        break;
+                    }
+                } else if (list1.isEmpty()) {
+                    for (TreeNode node : list2) {
+                        if (node.left != null) list1.add(node.left);
+                        if (node.right != null) list1.add(node.right);
+                    }
+                    if (list1.isEmpty() == false) {
+                        list2 = new ArrayList<TreeNode>();
+                    } else {
+                        break;
+                    }
+                }
+            }
+            int answer = 0;
+            for (TreeNode node : list1) {
+                answer += node.val;
+            }
+            for (TreeNode node : list2) {
+                answer += node.val;
+            }
+            return answer;
+        }
+    
+    
 }
