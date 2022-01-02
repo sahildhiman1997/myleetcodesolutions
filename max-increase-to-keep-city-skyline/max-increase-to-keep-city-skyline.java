34b0cbdc01926b33512241838e0e5ce010c49f7d
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-01-02 11:44:37 +0530

    Time: 2 ms (24.22%), Space: 40.9 MB (15.59%) - LeetHub

--- max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -1,32 +1,25 @@
-class Solution {
-    public int maxIncreaseKeepingSkyline(int[][] grid) {
-      int sum = 0;
+
+public class Solution {
+
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+        int sum = 0;
         int n = grid.length;
         int[] maxRows = new int[n];
         int[] maxcolumns = new int[n];
-        
-        for (int r =0; r<n;r++){
-            int maxx=0;
-            for(int c = 0;c<n;c++){
-                    maxx=Math.max(maxx, grid[r][c]);
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                maxRows[r]  = Math.max( maxRows[r], grid[r][c]);
+                maxcolumns[c]  = Math.max(maxcolumns[c], grid[r][c]);
             }
-            maxRows[r]=maxx;
         }
-        
-        for (int c =0; c<n;c++){
-            int maxx=0;
-            for(int r = 0;r<n;r++){
-                    maxx=Math.max(maxx, grid[r][c]);
-            }
-            maxcolumns[c]=maxx;
-        }
-        
-        for (int r =0; r<n;r++){  
-            for(int c = 0;c<n;c++){
-                     sum += Math.max(Math.min(maxRows[r], maxcolumns[c]), grid[r][c]) - grid[r][c];
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+
+                sum += Math.min(maxRows[r], maxcolumns[c]) - grid[r][c];
             }
         }
         return sum;
-        
+
     }
 }
