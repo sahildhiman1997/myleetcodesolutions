793bdaed6353d27eb433e246add501a9fafe1561
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-01-06 22:39:54 +0530

    Time: 2 ms (74.98%), Space: 41.3 MB (19.42%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,57 +1,18 @@
-class Trip {
-
-    int from;
-    int to;
-
-    public Trip(int passengers, int from, int to) {
-        this.from = from;
-        this.to = to;
-        this.passengers = passengers;
-    }
-
-    int passengers;
-
-}
-
 class Solution {
-   public boolean carPooling(int[][] trips, int capacity) {
-        int currentCapacity = 0;
-        List<Trip> alltrips = new ArrayList<Trip>();
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] stops = new int[1001];
         for(int[] trip: trips){
-            alltrips.add(new Trip(trip[0], trip[1], trip[2]));
+            stops[trip[1]]+=trip[0];
+            stops[trip[2]]-=trip[0];
         }
-       
-        List<Trip> notCompletedTrips =  Collections.synchronizedList(new ArrayList<>());
-       
-        alltrips.sort(new Comparator<Trip>() {
-            @Override
-            public int compare(Trip o1, Trip o2) {
-                if(o1.from-o2.from==0){
-                    return o1.to-o2.to;
-                }
-                return o1.from - o2.from;
+        int currentCapacity = 0;
+        for(int i = 0;i<1001;i++){
+currentCapacity+=stops[i];
+            if(currentCapacity>capacity){
+                return false;
             }
-        });
-       
-        for (int i = 0; i < alltrips.size() ; i++) {
-            for (Trip t : notCompletedTrips) {
-                if (t.to <= alltrips.get(i).from) {
-                    currentCapacity -= t.passengers;
-                    t.passengers=0;
-                    // notCompletedTrips.get(t);
-
-                }
-            }
-
-                currentCapacity += alltrips.get(i).passengers;
-                notCompletedTrips.add(alltrips.get(i));
-                if (currentCapacity > capacity) {
-                    return false;
-                }
-
         }
-
         return true;
-
+        
     }
 }
