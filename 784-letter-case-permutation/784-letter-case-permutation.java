9f7aff48ec92b411b3eef9b34e0fb802f8f4963b
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-02-04 16:50:02 +0530

    Time: 31 ms (9.19%), Space: 54.5 MB (5.11%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList();
+        
+        ans.add(s);
+        for(int pos = 0;pos<s.length();pos++){
+            
+            if(Character.isAlphabetic(s.charAt(pos))){
+                System.out.println("char found " +s.charAt(pos));
+                int size = ans.size();
+                
+                //for each string in the answer list, add the opposite case of the current alphabet 
+                for(int i = 0;i<size;i++){
+                    s = ans.get(i);
+                    StringBuilder str = new StringBuilder("");
+                        for(int c = 0;c<s.length();c++){
+                            if(pos==c){
+                                    if(Character.isUpperCase(s.charAt(pos))){
+                                    str.append(Character.toLowerCase(s.charAt(c)));
+                                }else{
+                                    str.append(Character.toUpperCase(s.charAt(c)));
+                                }
+                                continue;
+                            }
+                            str.append(s.charAt(c));
+                        }
+                    ans.add(str.toString());
+                            
+                 }
+            }
+        }
+        return ans;
+    }
+}
