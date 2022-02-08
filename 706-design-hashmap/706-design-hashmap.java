6d1050abe5c524582b1cfefe243f74bdad970475
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-02-08 15:50:59 +0530

    Time: 71 ms (29.82%), Space: 110.4 MB (5.54%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -6,14 +6,13 @@ class MyHashMap {
     }
     
     public void put(int key, int value) {
-        map[key]=value;
-        if(value==0) map[key]=-1;;
+        map[key]=value+1;
+        
     }
     
     public int get(int key) {
-        if(map[key]==0) return -1;
-        if(map[key]==-1) return 0;
-        return map[key];
+      
+        return map[key]-1;
     }
     
     public void remove(int key) {
