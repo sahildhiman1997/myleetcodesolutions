1581f6460a166b2db1cac613ebe88a8612912651
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-02-10 21:27:18 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (5.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,18 +1,32 @@
 class Solution {
     public int climbStairs(int n) {
-        
+        if(n<4) return n;
+ 
         // Pure recursion, TLE error.
-        // if(n<4) return n;
         // return climbStairs(n-1)+climbStairs(n-2);
         
         // Using additional space.
-        if(n<4) return n;
-        int[] ans = new int[n];
-        ans[1]=1;
-        ans[2]=2;
-        for(int i=3;i<n;i++){
-            ans[i]=ans[i-1]+ans[i-2];
+        // if(n<4) return n;
+        // int[] ans = new int[n];
+        // ans[1]=1;
+        // ans[2]=2;
+        // for(int i=3;i<n;i++){
+        //     ans[i]=ans[i-1]+ans[i-2];
+        // }
+        // return ans[n-1]+ans[n-2];
+        
+        
+//         can we reduce the space complexity more ?
+        int a = 1;
+        int b = 1;
+        for(int i=3;i<=n;i++){
+            int temp = a;
+            a=b;
+            b+=temp;
         }
-        return ans[n-1]+ans[n-2];
+        return a+b;
+        
+        
+        
     }
 }
