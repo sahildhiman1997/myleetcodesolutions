4079208c7740d3f62fc891592c81471c70e05705
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-02-15 17:47:51 +0530

    Time: 2 ms (99.90%), Space: 50.9 MB (69.58%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -6,13 +6,13 @@ class Solution {
         int ans = start+end;        
         
         for (int i = 1;i<values.length;i++){
-            if(end<=values[i]-i){
+            if(end<values[i]-i){
                 end = values[i]-i;
                 ans = Math.max(start+end, ans);
             }
             // System.out.println("start is "+start+", end is "+end+" at "+i);
          
-            if(start<=values[i]+i ){
+            if(start<values[i]+i ){
                 start=values[i]+i;
                 end = Integer.MIN_VALUE;
             }
