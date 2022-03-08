65129a21e34d98356c86dac4551a449cb270c712
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-08 09:24:27 +0530

    Time: 1 ms (46.18%), Space: 45.9 MB (28.39%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -15,30 +15,18 @@ public class Solution {
         ListNode tortoise = head;
         ListNode rabbit = head;
         
-        if(head == null || head.next == null){
+        if(head == null || head.next == null || head.next.next==null){
             return false;
         }
-        if(head.next.next==null) return false;
-        
         
         do{
-
-            
             if(rabbit.next!=null && rabbit.next.next!=null){
                 rabbit = rabbit.next.next;
                 tortoise = tortoise.next;
             }else{
                 return false;
             }
-            
-       
-        
         }while(tortoise!=rabbit);
-        
         return true;
-        
-        
-        
-        
     }
 }
