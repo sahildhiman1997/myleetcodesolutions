ba01c9114c4a8b196e443a29cac71f8fe7bb5698
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-13 14:31:54 +0530

    Time: 19 ms (41.57%), Space: 46.3 MB (6.96%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null) return null;
+ 
+        Node current = head;
+        
+        while(current!=null){
+            Node n = new Node(current.val);
+            n.next = current.next;
+            n.random = current.random;
+            current.next = n;
+            current = n.next;
+        }
+        
+        // while(head!=null){
+        //     int k = (head.random==null)?-1:head.random.val;
+        //     System.out.println(head.val+", "+k);
+        //     head = head.next;
+        // }
+        
+  
+        Node n1 = head.next;
+        
+       
+        current = n1;
+        while(current.next!=null){
+            if(current.random!=null) current.random = current.random.next;
+            current = current.next.next;
+        }
+        if(current.random!=null) current.random = current.random.next;
+                
+        current = head;
+        
+        while(current!=null){
+            System.out.println(current.val+", is the value at current, and next is"+current.next.val);
+            Node copy = current.next;
+            Node n = copy.next;
+            
+            if(copy.next!=null) copy.next = copy.next.next;
+            current.next = n;
+            current = current.next;
+            
+        }
+        return n1;
+        
+    }
+}
