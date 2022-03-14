e131aa9bd4843bc2fcf14f04517dc1e22bdc06f6
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-14 18:44:44 +0530

    Time: 26 ms (5.36%), Space: 44 MB (34.47%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String simplifyPath(String path) {
+        
+        String[] pointers = path.split("/");
+        
+        Stack<String> stack =  new Stack<String>();
+        
+        for(String p: pointers){
+            if(p.equalsIgnoreCase("..")){
+                
+                 if(stack.isEmpty()==false) stack.pop();
+            } 
+            
+            else if(p!="" && p.equals(".")==false && p.equals("/")==false){
+                stack.push(p);
+            }
+        } 
+        
+        StringBuilder ans = new StringBuilder("");
+        
+        while(!stack.isEmpty()){
+            String k = stack.pop();
+            System.out.println(k);
+            ans.insert(0,"/"+k);
+        }
+        
+        if(ans.toString()=="") return "/";
+        
+        return ans.toString();
+    }
+}
