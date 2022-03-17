69f1766cd2c6dd1ea9b76c55baa79831655d497c
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-17 23:52:42 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (40.09%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+       int ans = 0;
+       ans+=helper(s, 0, s.length());
+       return ans;
+    }
+    
+    int helper(String s, int start , int end){
+        int count = 0;
+        int ans = 0;
+        for(int i = start;i<end;i++){
+            if(s.charAt(i)=='('){
+                count++;
+            }else if(s.charAt(i)==')'){
+                count--;
+                if(count ==0){
+                    if(i-start == 1){
+                        ans++;
+                        start=i+1;
+                    }else{
+                    ans+=2*helper(s, start+1, i);
+                        start=i+1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
