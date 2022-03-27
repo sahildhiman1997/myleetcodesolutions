7cf11081b7e62c8e3f73ae5eb7452febd6ee0699
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-27 18:52:59 +0530

    Time: 3 ms (75.98%), Space: 47.8 MB (26.36%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        
         int ans = 0;
-        for(int i =0;i<grid.length;i++){
+        
+        for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
-                
                 if(grid[i][j]==1){
-
-                    ans = Math.max(ans, helper(grid, i, j));
+                    ans = Math.max(ans, getSize(grid, i, j));
                 }
             }
         }
+       
         return ans;
     }
-    int helper(int[][] grid, int i ,int c){
-        
-        if(grid[i][c]!=1) return 0;
-        grid[i][c]=-1;
-        
-        int count = 1;
-        
-        if(i<grid.length-1){count+=helper(grid, i+1, c);};
-        if(i>0)            {count+=helper(grid, i-1, c);};
-        if(c<grid[0].length-1){count+=helper(grid, i, c+1);};
-        if(c>0){ count+=helper(grid, i, c-1);};
-        return count;
-        
-        
-        
-    }
+    
+   int getSize(int[][] grid,int i, int j){
+       int size = 1;
+       grid[i][j]=0;
+       
+       if(i<grid.length-1 && grid[i+1][j]==1) size+=getSize(grid, i+1,j);
+       if(i>0 && grid[i-1][j]==1) size+=getSize(grid, i-1,j);
+       
+       if(j<grid[0].length-1 && grid[i][j+1]==1) size+=getSize(grid, i,j+1);
+       if(j>0 && grid[i][j-1]==1) size+=getSize(grid, i,j-1);
+       
+       return size;
+   }
 }
