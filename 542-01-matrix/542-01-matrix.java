9e0866287a49e1fd98b2ee85ec50d383912a602f
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-30 11:58:18 +0530

    Time: 7 ms (93.20%), Space: 73.5 MB (28.14%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,48 +1,24 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        for(int r=0;r<mat.length;r++){
-            for(int c=0;c<mat[0].length;c++){
-           
-                
-               if(mat[r][c]!=0){
-                    mat[r][c] = mat.length+mat[0].length;
-                   if(r==0 &&c>0) {
-                       mat[r][c]= mat[r][c-1]+1;
-                   }
-                   
-                   if(r>0){
-                        mat[r][c]=mat[r-1][c]+1;
-                        if(c>0){
-                            mat[r][c]= Math.min(mat[r][c], mat[r][c-1]+1);
-                        }   
-                    }
-               }
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[0].length;j++){
+                if(mat[i][j]!=0){
+                    int left = i>0?mat[i-1][j]+1:100000;
+                    int up = j>0?mat[i][j-1]+1:100000;
+                    mat[i][j]= Math.min(left, up);
+                }
             }
         }
-            
-        for(int r = mat.length-1;r>-1;r--){
-            for(int c = mat[0].length - 1;c>-1;c--){
-           
-                
-                
-                 if(mat[r][c]!=0){
-                     
-                     
-                     if(r==mat.length-1 && c<mat[0].length-1){
-                          mat[r][c]= Math.min(mat[r][c], mat[r][c+1]+1);
-                     }
-                     
-                    if(r<mat.length-1){
-                        mat[r][c]= Math.min(mat[r][c], mat[r+1][c]+1);
-                    }   
-                    if(c<mat[0].length-1){
-                        mat[r][c]= Math.min(mat[r][c], mat[r][c+1]+1);
-                    }   
-               }
-
-        }
+        
+        for(int i =mat.length-1;i>=0;i--){
+            for(int j = mat[0].length-1;j>=0;j--){
+                if(mat[i][j]!=0){
+                    int right = i<mat.length-1?mat[i+1][j]+1:100000;
+                    int down = j<mat[0].length-1?mat[i][j+1]+1:100000;
+                    mat[i][j]= Math.min(mat[i][j], Math.min(right, down));
+                }
+            }
         }
         return mat;
     }
 }
-   
