37291e4e31a71e92285fdf92a422a2cdcece70bd
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-03-31 17:36:06 +0530

    Time: 7 ms (6.07%), Space: 45.1 MB (25.07%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,46 +1,37 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         
-        
         List<List<Integer>> ans = new ArrayList();
-        if(nums.length==0) return ans;
+        ans.add(new ArrayList<Integer>());
         
-        ans.add(new ArrayList(){{add(nums[0]);}});
-        
-                
-//      for each number that we have been provided
-        for(int i=1;i<nums.length;i++){
-            
-//          get the current size of the answer
-            int size = ans.size();
-            
-//          for each list in the answer as of now, add the current number at each position
-            for(int j = 0;j<size;j++){
-                List<Integer> l = ans.get(j);
-                
-                
-//              copy the list into temporary list and then append the list to the answer,
-                // not once but the current size of each list, that is 'i'
-                
-                int pos = 0;
-                while(pos<l.size()){
-                    List<Integer> temp = new ArrayList();
-                    for(int e = 0;e<l.size();e++){
-                        if(pos==e){
-                            temp.add(nums[i]);
-                        }
-
-                        temp.add(l.get(e));
+        for(int i: nums){
+            System.out.println(ans.toString());
+            int ansSize = ans.size();
+            for(int nthList= 0;nthList<ansSize;nthList++){
+                List<Integer> l1 = ans.get(nthList);
+                for(int blankSpace=0;blankSpace<l1.size();blankSpace++){
+                    
+                    // deep copy l into l2
+                    List<Integer> l2 = new ArrayList();
+                    for(int p = 0;p<l1.size();p++){
+                        if(p==blankSpace) l2.add(i);
+                        l2.add((int) l1.get(p));
                     }
-                   
-                    ans.add(temp);
-                    pos++;
+                    
+                    ans.add(l2);
                 }
-                 l.add(nums[i]);
+                ans.get(nthList).add(i);
                 
             }
+            
+            
+            
         }
+        
+        
+        
         return ans;
+      
         
     }
 }
