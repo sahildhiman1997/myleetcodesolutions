5e5ff21a2f50ce145e8c4e1f9f5002851310b10b
Sahil Dhiman <sahildhiman1997@gmail.com>
2022-04-03 23:29:23 +0530

    Time: 1 ms (82.52%), Space: 42.8 MB (26.27%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,11 +1,13 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int ans = 0;
+        int ans = n&1;
         for(int i = 0;i<31;i++){
-            ans+= n&1;
-            n= n>>1;
+            
+            n= n>>>1;
             ans = ans<<1;
+            ans|= n&1;
+            
         }
        if(n<0){
            ans+=1;
